743f83a5809000cdd3ff1c940cc54353a6db84b2
Abay Reushenov <abay.reushenov@gmail.com>
2020-09-28 16:28:24 +0300

    Hide element by clicking on button

---
+++ HideMeApp.js
@@ -0,0 +1,21 @@
+export default class App extends Component {
+  state = {
+   show: true
+  }
+  
+  fun() {
+    this.setState({
+      show: !http://this.state.show
+    })
+  }
+  
+  render() {
+    return (
+      <>
+      {http://this.state.show ? (<h1>Hide me</h1>) :null }
+      <button onClick={()=> http://this.fun()}>Click</button>
+      </>
+     );
+  }
+  
+}
